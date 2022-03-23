43ba15a4cd930749d5744df05062264411a9dbad
Daniel Sias <dan.sias@gmail.com>
2022-03-23 00:50:58 -0400

    Game board

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,17 @@
+<script setup>
+import GameRow from "./GameRow.vue"
+import { computed } from "vue"
+import { useStore } from "vuex"
+const store = useStore()
+const guesses = computed(() => store.state.guesses)
+</script>
+
+<template>
+  <div class="mt-8 mx-auto">
+    <div class="flex flex-col justify-center items-center">
+      <div v-for="row in guesses" :key="row">
+        <GameRow :row="row" />
+      </div>
+    </div>
+  </div>
+</template>
